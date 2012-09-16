add04225f4fabf9acbf1a6d604bf1c0105c2eb47
Maria <maria@laptopi.home.lan>
2012-09-16 21:08:49 +0300

    Some changes

--- Gemfile
+++ Gemfile
@@ -8,6 +8,9 @@ gem 'rails', '3.2.8'
 group :development, :test do
   gem 'sqlite3', '1.3.5'
   gem 'rspec-rails', '2.11.0'
+  gem 'guard-spork', '0.3.2'
+  gem 'spork', '0.9.0'
+  gem 'rb-inotify', '~> 0.8.8'
 end
 
 group :production do
--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,6 +2,8 @@ require 'spec_helper'
 
 describe "Static pages" do
 
+let(:base_title) { "Ruby on Rails Tutorial Sample App" }
+
   describe "Home page" do
 
     it "should have the content 'Sample App'" do
@@ -10,7 +12,7 @@ describe "Static pages" do
     end
 		it "should have the right title to Home" do
 				visit '/static_pages/home'
-				page.should have_selector('title',text: " | Home")
+				page.should have_selector('title',text: "#{base_title} | Home")
 		end
 
 	end
@@ -24,7 +26,7 @@ describe "Static pages" do
 
 		it "should have the right title to Help" do
 				visit '/static_pages/help'
-				page.should have_selector('title',text: " | Help")
+				page.should have_selector('title',text: "#{base_title} | Help")
 		end
 
   end
@@ -38,7 +40,21 @@ describe "Static pages" do
 
 		it "should have the right title to About" do
 				visit '/static_pages/about'
-				page.should have_selector('title',text: " | About Us")
+				page.should have_selector('title',text: "#{base_title} | About Us")
+		end
+
+  end
+
+	describe "Contact" do
+
+    it "should have the content 'Contact'" do
+    	visit '/static_pages/contact'
+    	page.should have_selector('h1',text: 'Contact')
+    end
+
+		it "should have the right title to Contact" do
+				visit '/static_pages/contact'
+				page.should have_selector('title',text: "#{base_title} | Contact")
 		end
 
   end
