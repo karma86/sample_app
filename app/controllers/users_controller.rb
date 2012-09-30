d79595fc96ad157fe82afe52edff25a78ee3d61b
Maria Karoutsou <mariakaroutsou@gmail.com>
2012-09-30 19:50:19 +0300

    Finish sign in-out

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,10 +11,12 @@ class UsersController < ApplicationController
 	def create
     @user = User.new(params[:user])
     if @user.save
+      sign_in @user
+      flash[:success] = "Welcome to the Sample App!"
       redirect_to @user
-			flash[:success] = "Welcome to the Sample App!"
     else
       render 'new'
     end
   end
+
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -25,6 +25,8 @@ describe User do
 	it { should respond_to(:password_digest) }
   it { should respond_to(:password) }
   it { should respond_to(:password_confirmation) }
+  it { should respond_to(:remember_token) }
+  it { should respond_to(:authenticate) }
 
   it { should be_valid }
 
@@ -56,6 +58,13 @@ describe User do
         	@user.should_not be_valid
       	end
     	end
+
+	describe "remember token" do
+    before { @user.save }
+    its(:remember_token) { should_not be_blank }
+  end
+
+
   end
 
 	describe "when email format is valid" do
