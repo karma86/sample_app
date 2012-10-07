7bb1cd948331eda7d1850922c17e6a0bdadba565
Maria Karoutsou <mariakaroutsou@gmail.com>
2012-10-07 16:35:38 +0300

    Friendly redirects on sign in

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
 before_filter :signed_in_user, only: [:edit, :update]
+before_filter :correct_user, only: [:edit, :update]
 
 
 	def show
@@ -38,12 +39,17 @@ before_filter :signed_in_user, only: [:edit, :update]
 
 private
 
-    def signed_in_user
-			unless signed_in?
-      	flash[:notice] = "Please sign in."
+  def signed_in_user
+	    unless signed_in?
+	      store_location
+        flash[:notice] = "Please sign in."
 				redirect_to signin_url
 			end
     end
 
+  def correct_user
+    @user = User.find(params[:id])
+    redirect_to(root_path) unless current_user?(@user)
+  end
 
 end
